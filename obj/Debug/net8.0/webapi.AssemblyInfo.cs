2b12e3ee4518a4cc49da1a5ad8893274b1cdfd03
vicktoh <ayomidevictor349@gmail.com>
2024-01-22 09:26:26 +0100

    changes

--- obj/Debug/net8.0/webapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/webapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd50a8921c356474379ce3fd49914063bbe84ffb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f5fec1a0557affb6c199dfe9a8afa28e357296")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
